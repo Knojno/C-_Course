e891bbbd22447c9f39ea3e7442a82691422e2b35
renato <renato96souza@outlook.com>
2021-03-26 20:50:08 -0300

    Class UPCASTING AND DOWNCASTING

--- Heranca_polifornismo/Heranca_polifornismo/Program.cs
+++ Heranca_polifornismo/Heranca_polifornismo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Heranca_polifornismo.Entities;
 
 namespace Heranca_polifornismo
 {
@@ -6,7 +7,29 @@ namespace Heranca_polifornismo
     {
         static void Main(string[] args)
         {
-            
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            //UPCASTING
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+            //DOWNCASTING -> usar se realmente for nescessario
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+            if (acc3 is BusinessAccount)
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3;
+                BusinessAccount acc5 = acc3 as BusinessAccount;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan");
+            }
+            if (acc3 is SavingsAccount)
+            {
+                SavingsAccount acc5 = (SavingsAccount)acc3;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update");
+            }
         }
     }
 }
